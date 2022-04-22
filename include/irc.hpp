e6574bf086a8ed0cd6eeef46e4439559b621886a
Manon Berne <mberne@student.42lyon.fr>
2022-04-22 18:00:50 +0200

    beggining

---
+++ include/irc.hpp
@@ -0,0 +1,9 @@
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <poll.h>
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "irc.hpp"
+
+int main(int ac, char **av)
+{
+	if (ac != 3)
+		return 1;
+	return 0;
+}
