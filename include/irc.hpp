f136c460180ef00981c7af398a61da2c95a31a06
Pierre Thomas <pthomas@student.42lyon.fr>
2022-05-05 13:56:56 +0200

    remove obselete directories

--- include/irc.hpp
+++
@@ -1,9 +0,0 @@
-#include <sys/socket.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <poll.h>
