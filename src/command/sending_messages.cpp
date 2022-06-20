0859c9c410f7522cc1c9ea4cef2286e947f6526f
Manon Berne <mberne@student.42lyon.fr>
2022-06-20 11:02:19 +0200

    add NOTICE cmd

--- src/command/sending_messages.cpp
+++ src/command/sending_messages.cpp
@@ -15,10 +15,10 @@ void	irc_notice(std::vector<std::string> cmd, Client* sender, Server* serv)
 		parseArg(cmd[1], target);
 		for (size_t i = 0; i < target.size(); i++)
 		{
-			if (serv->getChannel(target[i]))
-				serv->getChannel(target[i])->sendToClients(sender->getPrefix() + " NOTICE " + serv->getClient(target[i])->getNickname() + " " + cmd[2]);
-			else if (serv->getClient(target[i]))
-				sender->addToOutputBuffer(sender->getPrefix() + " NOTICE " + serv->getClient(target[i])->getNickname() + " " + cmd[2]);
+			if (serv->getClient(target[i]))
+				serv->getClient(target[i])->addToOutputBuffer(sender->getPrefix() + " NOTICE " + serv->getClient(target[i])->getNickname() + " :" + cmd[2]);
+			else if (serv->getChannel(target[i]))
+				serv->getChannel(target[i])->sendToClients(sender->getPrefix() + " NOTICE " + serv->getChannel(target[i])->getName() + " :" + cmd[2]);
 		}
 	}
 }
