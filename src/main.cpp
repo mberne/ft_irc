7c572796f70670618af0c211e85b28bd962b11ba
Pierre Thomas <pthomas@student.42lyon.fr>
2022-06-29 10:15:45 +0200

    unrelevant changes

--- src/main.cpp
+++ src/main.cpp
@@ -13,20 +13,19 @@ int main(int ac, char **av)
 		return EXIT_FAILURE;
 	}
 	
-	int serverPort = std::atoi(av[1]);
-	if (serverPort < 0 || serverPort > MAX_PORT || !av[2][0] || std::string(av[1]).find_first_not_of("0123456789") != std::string::npos)
+	size_t	serverPort = std::atoi(av[1]);
+	if (serverPort > MAX_PORT || !av[2][0] || std::string(av[1]).find_first_not_of("0123456789") != std::string::npos)
 	{
 		errno = EINVAL;
 		std::perror(SERV_NAME.c_str());
 		return EXIT_FAILURE;
 	}
 
-	Server server(serverPort, av[2]);
-
 	std::signal(SIGINT, &stop_handler);
 	std::signal(SIGQUIT, &stop_handler);
 	std::signal(SIGTERM, &stop_handler);
 
+	Server server(serverPort, av[2]);
 	try			{server.run();}
 	catch(...)	{server.stop(errno);}
 }
