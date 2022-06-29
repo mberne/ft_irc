75e08ac322f24f3bf0534b1be2c019707beaf881
Pierre Thomas <pthomas@student.42lyon.fr>
2022-06-29 16:51:48 +0200

    lil opti

--- src/command/channel_operations.cpp
+++ src/command/channel_operations.cpp
@@ -297,21 +297,24 @@ void	irc_invite(std::vector<std::string> cmd, Client* sender, Server* serv)
 		sender->addToOutputBuffer(ERR_NEEDMOREPARAMS(sender->getNickname(), cmd[0]));
 	else
 	{
-		if (!serv->getClient(cmd[1]))
+		Client*		client = serv->getClient(cmd[1]);
+		Channel*	channel = serv->getChannel(cmd[2]);
+
+		if (!client)
 			sender->addToOutputBuffer(ERR_NOSUCHNICK(sender->getNickname(), cmd[1]));
-		else if (!serv->getChannel(cmd[2]))
+		else if (!channel)
 			sender->addToOutputBuffer(ERR_NOSUCHCHANNEL(sender->getNickname(), cmd[2]));
-		else if (!serv->getChannel(cmd[2])->getClient(sender->getNickname()))
+		else if (!channel->getClient(sender->getNickname()))
 			sender->addToOutputBuffer(ERR_NOTONCHANNEL(sender->getNickname(), cmd[2]));
-		else if (serv->getChannel(cmd[2])->getClient(serv->getClient(cmd[1])->getNickname()))
+		else if (channel->getClient(client->getNickname()))
 			sender->addToOutputBuffer(ERR_USERONCHANNEL(sender->getNickname(), cmd[1], cmd[2]));
-		else if (serv->getChannel(cmd[2])->hasModes(CHANNEL_FLAG_I) && !serv->getChannel(cmd[2])->isOperator(sender))
+		else if (channel->hasModes(CHANNEL_FLAG_I) && !channel->isOperator(sender))
 			sender->addToOutputBuffer(ERR_CHANOPRIVSNEEDED(sender->getNickname(), cmd[2]));
 		else
 		{
 			sender->addToOutputBuffer(RPL_INVITING(sender->getNickname(), cmd[1], cmd[2]));
-			serv->getClient(cmd[1])->addToOutputBuffer(sender->getPrefix() + " " + cmd[0] + " " + cmd[1] + " :" + cmd[2]);
-			serv->getChannel(cmd[2])->addInvitedClient(serv->getClient(cmd[1]));
+			client->addToOutputBuffer(sender->getPrefix() + " " + cmd[0] + " " + cmd[1] + " :" + cmd[2]);
+			channel->addInvitedClient(client);
 		}
 	}
 }
