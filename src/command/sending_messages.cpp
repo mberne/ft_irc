cf18e3e8db5ba55e60ba00e4da4df3a777bbe611
Pierre Thomas <pthomas@student.42lyon.fr>
2022-06-29 16:26:41 +0200

    lil opti

--- src/command/sending_messages.cpp
+++ src/command/sending_messages.cpp
@@ -4,7 +4,11 @@ void	irc_privmsg(std::vector<std::string> cmd, Client* sender, Server* serv)
 {
 	std::vector<std::string>	target;
 
-	if (cmd.size() > 2)
+	if (cmd.size() == 1)
+		sender->addToOutputBuffer(ERR_NOTEXTTOSEND(sender->getNickname()));
+	else if (cmd.size() == 2)
+		sender->addToOutputBuffer(ERR_NORECIPIENT(sender->getNickname(), cmd[0]));
+	else
 	{
 		parseArg(cmd[1], target);
 		for (size_t i = 0; i < target.size(); i++)
@@ -31,10 +35,6 @@ void	irc_privmsg(std::vector<std::string> cmd, Client* sender, Server* serv)
 			}
 		}
 	}
-	else if (cmd.size() == 2)
-		sender->addToOutputBuffer(ERR_NOTEXTTOSEND(sender->getNickname()));
-	else if (cmd.size() == 1)
-		sender->addToOutputBuffer(ERR_NORECIPIENT(sender->getNickname(), cmd[0]));
 }
 
 void	irc_notice(std::vector<std::string> cmd, Client* sender, Server* serv)
@@ -51,12 +51,12 @@ void	irc_notice(std::vector<std::string> cmd, Client* sender, Server* serv)
 				serv->addLog("from: " + SERV_NAME + " to: " + target[i] + "\n" + cmd[2], LOG_MESSAGE);
 				serv->getClient(target[i])->addToOutputBuffer(SERV_NAME + " NOTICE " + serv->getClient(target[i])->getNickname() + " :" + cmd[2]);
 			}
-			else if (target[i][0] != '#' && serv->getClient(target[i]))
+			else if (serv->getClient(target[i]))
 			{
 				serv->addLog("from: " + sender->getPrefix() + " to: " + target[i] + "\n" + cmd[2], LOG_MESSAGE);
 				serv->getClient(target[i])->addToOutputBuffer(sender->getPrefix() + " NOTICE " + serv->getClient(target[i])->getNickname() + " :" + cmd[2]);
 			}
-			else if (target[i][0] == '#' && serv->getChannel(target[i]) && serv->getChannel(target[i])->ableToTalk(sender))
+			else if (serv->getChannel(target[i]) && serv->getChannel(target[i])->ableToTalk(sender))
 			{
 				serv->addLog("from: " + sender->getPrefix() + " to: " + target[i] + "\n" + cmd[2], LOG_MESSAGE);
 				serv->getChannel(target[i])->sendToClients(sender->getPrefix() + " NOTICE " + serv->getChannel(target[i])->getName() + " :" + cmd[2], sender);
